515d36c988eb00cfe393a03be696127698e76a19
levy <lnjiru43@gmail.com>
2023-02-07 01:49:02 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -3,9 +3,11 @@
 #     { id: 3, hello: "Moon" }, 
 #     { id: 4, hello: "Learner" }]
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
+p [1,2,3]
+puts "Hello World!"
+puts "Hello World!"
+puts "Hello World!"
+print "Pass this test, please."
 # p [1,2,3]
 
 # def add
